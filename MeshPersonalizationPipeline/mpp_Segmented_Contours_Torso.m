f1fe542fafcac32226cef751459dbb40265a705c
Federico <155232271+FedeOmb@users.noreply.github.com>
2026-04-12 15:19:27 +0200

    aggiunti path windows e linux

--- MeshPersonalizationPipeline/mpp_Segmented_Contours_Torso.m
+++ MeshPersonalizationPipeline/mpp_Segmented_Contours_Torso.m
@@ -15,9 +15,12 @@ end
 
 %%
 mppOption pathfull
-setenv('PATH', ['/home/federico/TORSO-MPP/.venvmpp/bin', pathsep, getenv('PATH')]);
-system(['"/home/federico/TORSO-MPP/.venvmpp/bin/python" "', pathfull, 'PreTrained/torso_segmentor.py"', ' --dir_img ', Fullfile('mpp', 'torso-images'),...
-  ' --model_path ', fullfile(pathfull, 'PreTrained', 'N15_cycle_4.pt')]);
+%LINUX PATH
+%setenv('PATH', ['/home/federico/TORSO-MPP/.venvmpp/bin', pathsep, getenv('PATH')]);
+% WINDOWS PATH
+setenv('PATH', ['C:\Users\fedeo\Desktop\digital-twin-framework-camps\TORSO-MPP\.mppvenv\Scripts', pathsep, getenv('PATH')]);
+%system(['"/home/federico/TORSO-MPP/.venvmpp/bin/python" "', pathfull, 'PreTrained/torso_segmentor.py"', ' --dir_img ', Fullfile('mpp', 'torso-images'),' --model_path ', fullfile(pathfull, 'PreTrained', 'N15_cycle_4.pt')]);
+system(['"C:\Users\fedeo\Desktop\digital-twin-framework-camps\TORSO-MPP\.mppvenv\Scripts\python" "', pathfull, 'PreTrained\torso_segmentor.py"', ' --dir_img ', Fullfile('mpp', 'torso-images'),' --model_path ', fullfile(pathfull, 'PreTrained', 'N15_cycle_4.pt')]);
 
 %%
 BC0 = BS;
